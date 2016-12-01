ce999b6a11ba4a94d203f80edb2e3488b2b5115a
Andreas Scheuring <andreas.scheuring@de.ibm.com>
2016-12-01 10:04:48 +0100

    Neutron Mechanism Driver for DPM
    The dpm mechanism driver ensures, that the following
    attributes are part of the ports *vif_details* dictionary:
    
    VLAN networking
    * vlan
    * vlan_mode
    * vswitch_id
    
    Flat networking
    * vswitch_id
    
    If multiple vswitch_ids per physical network are supplied
    by the agent, the mechansim driver always picks the first
    one. This restriction might get lifted in future releases.
    
    Change-Id: I7df553e2486900a00f68f4b6753fa4257da93380
    Partial-Bug: #1646095

---
+++ networking_dpm/ml2/mech_dpm.py
@@ -0,0 +1,96 @@
+# Copyright (c) 2016 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log import log
+
+from neutron._i18n import _LW
+from neutron.extensions import portbindings
+from neutron.plugins.common import constants as p_constants
+from neutron.plugins.ml2 import driver_api as api
+from neutron.plugins.ml2.drivers import mech_agent
+
+LOG = log.getLogger(__name__)
+
+AGENT_TYPE_DPM = 'DPM agent'
+VIF_TYPE_DPM = 'dpm'
+VIF_DETAILS_VSWITCH_ID = 'vswitch_id'
+VLAN_MODE = 'vlan_mode'
+VLAN_MODE_INBAND = 'inband'
+
+
+class DPMMechanismDriver(mech_agent.SimpleAgentMechanismDriverBase):
+    """Attach to networks using DPM L2 Agent.
+
+    The DPMMechanismDriver integrates the ml2 plugin with the
+    DPM L2 agent. Port binding with this driver requires the
+    dpm agent to be running on the port's host, and that agent
+    to have connectivity to at least one segment of the port's
+    network.
+    """
+
+    def __init__(self):
+        super(DPMMechanismDriver, self).__init__(
+            AGENT_TYPE_DPM,
+            VIF_TYPE_DPM,
+            {portbindings.CAP_PORT_FILTER: False})
+
+    def get_allowed_network_types(self, agent):
+        return [p_constants.TYPE_FLAT, p_constants.TYPE_VLAN]
+
+    def get_mappings(self, agent):
+        return agent.get('configurations', {}).get('adapter_mappings', {})
+
+    def check_vlan_transparency(self, context):
+        """DPM driver vlan transparency support."""
+        return False
+
+    def try_to_bind_segment_for_agent(self, context, segment, agent):
+        if not self.check_segment_for_agent(segment, agent):
+            return False
+
+        physnet = segment['physical_network']
+        vswitch_ids = self.get_mappings(agent)[physnet]
+        if not vswitch_ids:
+            LOG.warning(_LW("No vswitch_ids found in agents %(agent)s mapping "
+                            "for physical network %(net)s."), {'agent': agent,
+                                                               'net': physnet})
+            return False
+        if len(vswitch_ids) > 1:
+            LOG.warning(_LW("More than one vswitch_id for physical network "
+                            "%(net)s on agent %(agent)s found but only a "
+                            "single vswitch_id is supported. Therefore the "
+                            "first one is being chosen!"), {'agent': agent,
+                                                            'net': physnet})
+        # TODO(andreas_s): In the first release only a single adapter
+        # per physical network is supported
+        vswitch_id = vswitch_ids[0]
+
+        vif_details_segment = self.vif_details
+        vif_details_segment[VIF_DETAILS_VSWITCH_ID] = vswitch_id
+
+        network_type = segment[api.NETWORK_TYPE]
+        if network_type == p_constants.TYPE_VLAN:
+            vlan_id = segment[api.SEGMENTATION_ID]
+            vif_details_segment[portbindings.VIF_DETAILS_VLAN] = vlan_id
+            # In the initial release only inband VLANs are supported.
+            # Nova uses this field to setup the VLANs accordingly.
+            vif_details_segment[VLAN_MODE] = VLAN_MODE_INBAND
+
+        LOG.debug("DPM vif_details added to context binding: %s",
+                  vif_details_segment)
+        context.set_binding(segment[api.ID], self.vif_type,
+                            vif_details_segment)
+        return True
