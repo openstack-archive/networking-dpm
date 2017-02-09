136cc458362ed27e37e142f5d2deea61edff0886
Andreas Scheuring <andreas.scheuring@de.ibm.com>
2017-02-09 10:43:51 +0100

    Add q-dhcp and q-meta to devstack single node
    This patch enables running an ovs-agent in parallel to the dpm-agt.
    This allows spawning an Neutron DHCP server on the singlenode.
    
    Change-Id: Ie65093c40bd2db800d6d7254052728eae2c0aeb7

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -12,6 +12,50 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+# Save trace setting
+XTRACE=$(set +o | grep xtrace)
+set +o xtrace
 
-dir=${GITDIR['networking-dpm']}
-source $dir/devstack/dpm_agent
+DPM_AGENT_CONF="/etc/neutron/plugins/ml2/dpm_agent.ini"
+#DPM_AGENT_CONF="${Q_PLUGIN_CONF_PATH}/dpm_agent.ini"
+DPM_AGENT_BINARY="${NEUTRON_BIN_DIR}/neutron-dpm-agent"
+
+# check for service enabled
+if is_service_enabled q-dpm-agt; then
+
+    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        # Set up system services
+        # no-op
+        :
+
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of service source
+
+        setup_develop $DEST/networking-dpm
+        iniset /$DPM_AGENT_CONF securitygroup firewall_driver noop
+
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        :
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        # Initialize and start the template service
+        run_process q-dpm-agt "$DPM_AGENT_BINARY --config-file $NEUTRON_CONF --config-file /$DPM_AGENT_CONF"
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # Shut down template services
+        stop_process q-dpm-agt
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # Remove state and transient data
+        # Remember clean.sh first calls unstack.sh
+        # no-op
+        :
+    fi
+fi
+
+# Restore xtrace
+$XTRACE
