640d60a1b7c99907ae15cafd925c9610e6589912
Andreas Scheuring <andreas.scheuring@de.ibm.com>
2017-02-16 08:25:31 +0100

    Preceise error handling on get_connected_nics fail
    When the call "vswitch.get_connected_nics" fails, a warning ha
    been logged an "sys.exit" being triggered.
    
    This patch adds a comment why going with sys.exit over an exception
    and turns the LOG message to loglevel 'error'.
    
    Change-Id: Ie35f01488846bfa1ccd0b3ee0cab74cce7b53d13

--- networking_dpm/ml2/dpm_neutron_agent.py
+++ networking_dpm/ml2/dpm_neutron_agent.py
@@ -290,16 +290,20 @@ class DPMManager(amb.CommonAgentManagerBase):
                                   "Continuing...", nic)
             except zhmcclient.HTTPError:
                 # TODO(andreas_s): Check general HMC connectivity first
-                LOG.warning(_LW("Retrieving connected VNICs for DPM vSwitch "
-                                "%(vswitch)s failed. DPM vSwitch object is "
-                                "not available anymore. This can happen if "
-                                "the corresponding adapter got removed "
-                                "from the system or the corresponding "
-                                "hipersockets network got deleted. Please"
-                                "adjust the physical_adapter_mappings "
-                                "configuration accordingly and start the "
-                                "agent again. Agent terminated!"),
-                            {'vswitch': vswitch})
+                LOG.error(_LE(
+                    "An unrecoverable error occurred!"
+                    "Retrieving connected VNICs for DPM vSwitch "
+                    "%(vswitch)s failed. DPM vSwitch object is "
+                    "not available anymore. This can happen if "
+                    "the corresponding adapter got removed "
+                    "from the system or the corresponding "
+                    "hipersockets network got deleted. Please"
+                    "adjust the physical_network_adapter_mappings "
+                    "configuration accordingly and start the "
+                    "agent again. Agent terminated!"),
+                    {'vswitch': vswitch})
+                # Need to exit with sys.exit, as calling code catches
+                # exceptions
                 sys.exit(1)
         return devices
 
